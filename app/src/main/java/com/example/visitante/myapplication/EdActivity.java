a5fabd21e8d93c6a04596816a9060d68ab88e87a
darrubla <darrubla@uninorte.edu.co>
2018-01-22 16:42:59 -0500

    First class to learn about Android Studio shares with Git

---
+++ app/src/main/java/com/example/visitante/myapplication/EdActivity.java
@@ -0,0 +1,13 @@
+package com.example.visitante.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ed);
+    }
+}
